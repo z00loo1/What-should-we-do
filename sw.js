aabc52d6349c541553c9468c16b083987f7a9576
z00loo1 <61928085+z00loo1@users.noreply.github.com>
2026-08-10 07:31:38 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='arya-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','activities.js','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
